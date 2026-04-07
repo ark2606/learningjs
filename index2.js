a1c63f7fea78a498342e64d70d04a1590b92315d
ark2606 <ankash2609@gmail.com>
2026-04-07 10:30:34 +0530

    function in js

--- index2.js
+++ index2.js
@@ -1,4 +1,20 @@
-console.log("Hello, World!");
-console.log("Welcome to JavaScript programming.");
-console.log("Let's start coding!");
+// function in javascript
+function add(a, b) {return a + b;}  // function declaration
 
+console.log(add(2, 3))   // 5 // 
+
+function multiply(p,q){ // function expression
+    return p*q
+}
+console.log(multiply(4,5)) // 20    
+const subtract = (x,y) => x-y // arrow function
+console.log(subtract(10, 4)) // 6   
+
+const hello = () => "Hello world!" // arrow function with implicit return
+console.log(hello()) // "Hello world!"
+
+const printMyname = name => "My name is " + name  // arrow function with single parameter and implicit return
+console.log(printMyname("Ankit"))
+
+const square = function(n) { return n*n} // function expression assigned to a variable
+console.log(square(6))
